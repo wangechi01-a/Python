09c47a378443f964ab2bb9f10c63a21ecde68247
wangechi01-@ <wangechieunice0@gmail.com>
2024-06-14 20:53:54 +0300

    projects

--- if_statements.py
+++ if_statements.py
@@ -1,59 +1,59 @@
-# # Example 1
-# kenyan = ["smocha","githeri","ugali"]
-# tanzanian = ["pilau","biryani","viazi"]
-# ugandan = ["matoke","ngumu","mandizi"]
+# Example 1
+kenyan = ["smocha","githeri","ugali"]
+tanzanian = ["pilau","biryani","viazi"]
+ugandan = ["matoke","ngumu","mandizi"]
 
 
-# dish = input("Enter a dish name: ")
-# if dish in kenyan:
-#     print("kenyan")
-# elif dish in tanzanian:
-#     print("tanzanian")
-# elif dish in ugandan:
-#     print("ugandan")
-# else:
-#     print("Based on my current knowledge i do not know which dish is",dish)
+dish = input("Enter a dish name: ")
+if dish in kenyan:
+    print("kenyan")
+elif dish in tanzanian:
+    print("tanzanian")
+elif dish in ugandan:
+    print("ugandan")
+else:
+    print("Based on my current knowledge i do not know which dish is",dish)
 
 
-# Example 2
+Example 2
     
-# india = ["mumbai", "banglore", "chennai", "delhi"]
-# pakistan = ["lahore","karachi","islamabad"]
-# bangladesh = ["dhaka", "khulna", "rangpur"]
+india = ["mumbai", "banglore", "chennai", "delhi"]
+pakistan = ["lahore","karachi","islamabad"]
+bangladesh = ["dhaka", "khulna", "rangpur"]
 
-# city = input("Enter the city belongs to: ")
+city = input("Enter the city belongs to: ")
 
-# if city in india:
-#     print(f"{city} in india")
-# elif city in pakistan:
-#     print(f"{city} in pakistan")
-# elif city in bangladesh:
-#     print(f"{city} in bangladesh")
+if city in india:
+    print(f"{city} in india")
+elif city in pakistan:
+    print(f"{city} in pakistan")
+elif city in bangladesh:
+    print(f"{city} in bangladesh")
 
-# else:
-#     print("I can't tell which {city} sorry")
+else:
+    print("I can't tell which {city} sorry")
 
 
-# city1 = input("Enter city 1: ")
-# city2 = input("Enter city 2: ")
+city1 = input("Enter city 1: ")
+city2 = input("Enter city 2: ")
 
-# if city1 in india and city2 in india:
-#     print("Both cities are in india")
+if city1 in india and city2 in india:
+    print("Both cities are in india")
 
-# elif city1 in pakistan and city2 in pakistan:
-#     print("Both cities are in pakistan ")
+elif city1 in pakistan and city2 in pakistan:
+    print("Both cities are in pakistan ")
 
-# elif city1 in bangladesh and city2 in bangladesh:
-#     print("Both cities in bangladesh")
+elif city1 in bangladesh and city2 in bangladesh:
+    print("Both cities in bangladesh")
 
-# else:
-#     print("They don't belong to same country")
+else:
+    print("They don't belong to same country")
 
-# # Example 3
+# Example 3
 
-# 1. Ask user to enter his fasting sugar level
-# 2. If it is below 80 to 100 range then print that sugar is low
-# 3. If it is above 100 then print that it is high otherwise print that it is norma
+1. Ask user to enter his fasting sugar level
+2. If it is below 80 to 100 range then print that sugar is low
+3. If it is above 100 then print that it is high otherwise print that it is norma
 sugar = input("Enter fasting sugar level: ")
 sugar = float (sugar)
 
